05beb03701822f50916f767fd29a6bf0b9479e61
Jorge Miranda <jorge.miranda@sefin.fortaleza.ce.gov.br>
2023-11-17 16:14:44 -0300

    first commit

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Tutorial js',
+        productNumber: '0001',
+        playmantStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'Tutorial js',
+        productNumber: '0001',
+        playmantStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'Tutorial flex-box',
+        productNumber: '0003',
+        playmantStatus: 'Paid',
+        status: 'Active'
+    },
+]
